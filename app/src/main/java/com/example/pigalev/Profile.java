138a1e55b736d74f71691d86d006e9ebed789327
unknown <@ngknn.local>
2023-03-03 16:12:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pigalev/Profile.java
@@ -0,0 +1,36 @@
+package com.example.pigalev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+    public  void nextMenu(View view)
+    {
+        startActivity(new Intent(this, Menu.class));
+    }
+
+    public  void nextHome(View view)
+    {
+        startActivity(new Intent(this, Main.class));
+    }
+
+    public void nextListen(View view)
+    {
+        startActivity(new Intent(this, Listen.class));
+    }
+
+    public void nextLogin(View view)
+    {
+        startActivity(new Intent(this, Login.class));
+    }
+}
---
+++ app/src/main/java/com/example/pigalev/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.pigalev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreen.this, Onboarding.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
