9aa296732451b99272ba93ddbeb72398cee54ab1
ПигалевЕД <1pigaleve@mail..ru>
2023-03-13 13:11:39 +0300

    Доработки

--- app/src/main/java/com/example/pigalev/Profile.java
+++ app/src/main/java/com/example/pigalev/Profile.java
@@ -1,19 +1,39 @@
 package com.example.pigalev;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class Profile extends AppCompatActivity {
 
     ImageView image;
     TextView tvName;
+
+    OutputStream outputStream;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +46,7 @@ public class Profile extends AppCompatActivity {
         image = findViewById(R.id.avatar);
         new AdapterMaskQuote.DownloadImageTask((ImageView) image)
                 .execute(Onboarding.avatar);
+
     }
 
     public  void nextMenu(View view)
@@ -52,4 +73,47 @@ public class Profile extends AppCompatActivity {
 
         startActivity(new Intent(this, Login.class));
     }
+
+    public void addImage(View view)
+    {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        someActivityResultLauncher.launch(photoPickerIntent);
+    }
+
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Bitmap bitmap = null;
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Uri selectedImage = result.getData().getData();
+                        try
+                        {
+                            bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+                        }
+                        catch (IOException e)
+                        {
+                            e.printStackTrace();
+                        }
+                        File filepath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                        File dir = new File(filepath.getAbsolutePath() + "/MyFiles/");
+                        dir.mkdirs();
+                        File file = new File(dir, System.currentTimeMillis() + ".jpg");
+                        try {
+                            outputStream = new FileOutputStream(file);
+                            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                            outputStream.flush();
+                            outputStream.close();
+                            Toast.makeText(Profile.this, "Изображение успешно сохранено", Toast.LENGTH_LONG).show();
+                        }
+                        catch (Exception e)
+                        {
+                            e.printStackTrace();
+                            Toast.makeText(Profile.this, "При сохранение изображения возникла ошибка!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+            });
 }
